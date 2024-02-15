1e1f4d4c5e52cbfb61dfdc0e5a3938a221599c06
Lord-Eler <80069231+Lord-Eler@users.noreply.github.com>
2024-02-15 17:56:48 +0100

    Updated generate_zip.bat

--- generate_zip.bat
+++ generate_zip.bat
@@ -2,17 +2,18 @@
 
 REM Create the directory
 mkdir Lebas__Ronan
+mkdir Lebas__Ronan\Lebas__Ronan
 
 REM Copy the directories into it
-xcopy /E /I cpp Lebas__Ronan\cpp
-xcopy /E /I swing Lebas__Ronan\swing
-xcopy /E /I docs Lebas__Ronan\docs
+xcopy /E /I cpp Lebas__Ronan\Lebas__Ronan\cpp
+xcopy /E /I swing Lebas__Ronan\Lebas__Ronan\swing
+xcopy /E /I docs Lebas__Ronan\Lebas__Ronan\docs
 
 REM Convert the README to PDF
 pandoc README.md -s -o README.html
 
 REM Copy the PDF into the directory
-copy README.html Lebas__Ronan\
+copy README.html Lebas__Ronan\Lebas__Ronan\
 
 REM Create a zip file of the directory
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('Lebas__Ronan', 'Lebas__Ronan.zip'); }"
